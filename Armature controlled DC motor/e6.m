e5e5ea6e8897b3cb939376b9b6c7739d3c62a6ce
Smit Shah <smitshah0885@gmail.com>
2020-04-28 12:10:10 +0530

    Add files via upload

---
+++ Armature controlled DC motor/e6.m
@@ -0,0 +1,41 @@
+%% EXPERIMENT NO: 6
+
+%{
+     Name: Smit Shah
+     PRN: 17070123090
+     Batch: EB-2
+%}
+
+clc
+clear all
+close all
+
+%% Q2. Simulate the SIMULINK model of DC motor designed for L=0.5 H, R=1 Ohm, Kt=Kb=0.01, B=0.1 N.m.s, J=0.01 Kg.m2/s2.
+
+Kb=0.01;
+J=0.01;
+B=0.1;
+R=1;
+Kt=0.01;
+sim('smit_e6')
+figure('name','Smit_step_response')
+plot(time,Speed)
+xlabel('Time')
+ylabel('Speed')
+title('DC motor with Step wave as input')
+axis([0 10 0 0.2])
+figure('name','Smit_ramp_response')
+plot(time,Speed1)
+xlabel('Time')
+ylabel('Speed')
+title('DC motor with Ramp wave as input')
+
+%% Conclusion
+
+%{
+   In this experiment firstly we have designed DC motor using SIMULINK.
+   Then we have defined the values of L, H, R, Kb, Kt, B and J in MATLAB.
+   Then we have called that block diagram in MATLAB using "sim" function
+   and plotted two graphs of "time vs speed" with step wave and ramp wave as
+   an input.
+%}
